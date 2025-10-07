8351616ef735f00ab1b815ac60f372e9df0c6c3d
real.ilia.dzneladze@gmail.com <real.ilia.dzneladze@gmail.com>
2025-10-07 08:37:39 +0200

    6, 7, 8 problems done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num < 0){
+        return "ERROR";
+    }
 
+    let cString = "";
+    for(let i = 0; i < num; i++){
+        cString += string;
+    }
+
+    return cString;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let len = string.length;
+    let revString = "";
+    for(let i = len - 1; i >= 0; i--){
+        revString += string[i];
+    }
 
+    return revString;
 };
 
 // Do not edit below this line
