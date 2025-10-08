3972c2104ae5c67b4f89660abc6b6fc69e00ad60
real.ilia.dzneladze@gmail.com <real.ilia.dzneladze@gmail.com>
2025-10-08 12:45:02 +0200

    9, 10, 11 problems done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let result = 0, start, end;
 
+    let numTypeOne = typeof(numOne);
+    let numTypeTwo = typeof(numTwo);
+
+    if((numTypeOne != "number") || (numTypeOne != numTypeTwo)){
+        return "ERROR";
+    }
+
+    if(!Number.isInteger(numOne) || !Number.isInteger(numTwo)){
+        return "ERROR";
+    }
+
+    if(numOne < 0 || numTwo < 0){
+        return "ERROR";
+    }
+
+
+    if(numOne > numTwo){
+        start = numTwo;
+        end = numOne;
+    } else{
+        start = numOne;
+        end = numTwo;
+    }
+
+    for(let i = start; i <= end; i++){
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
